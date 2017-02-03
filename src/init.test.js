8983bc467bf7c90e5984ba5abe26f38f26564394
Andreas Prummer <andreas.prummer@kununu.com>
2017-02-03 19:15:16 +0100

    Add linting airbnb style

--- src/init.test.js
+++ src/init.test.js
@@ -1,8 +1,9 @@
-import init from './init.js';
+import init from './init';
 
+// setup basic project structure
 describe('Init project structure', () => {
-
-    it('Inits with redux-observable adapter', () => {
-      expect(init()).toBe('hell yeah');
-    });
+  // setup project structure with specific technologies
+  it('Inits with redux-observable adapter', () => {
+    expect(init()).toBe('hell yeah');
+  });
 });
