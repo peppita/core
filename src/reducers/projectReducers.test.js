90705d2084f794612b4cdab307bfe7905026e661
Andreas Prummer <prumand@gmail.com>
2017-03-22 23:02:30 +0100

    Add basic test for projectReducer

---
+++ src/reducers/projectReducers.test.js
@@ -0,0 +1,9 @@
+import projectReducer from './projectReducer';
+
+
+describe('main project reducer actions', () => {
+  it('returns same state on unkown action', () => {
+    const testState = {test: 'testing'};
+    expect(projectReducer(testState, {type: 'asdf'})).toBe(testState);
+  });
+});
