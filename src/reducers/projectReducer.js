b9fc33fd833ac4d6203ea4fb748c618dd3fa01a6
Andreas Prummer <andreas.prummer@kununu.com>
2017-03-26 23:05:22 +0200

    Use immutable for state and add actions

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -1,8 +1,34 @@
 import Immutable from 'immutable';
 
-export default (state = Immutable.Map(), action) => {
+export const defaultConfig = {
+  peppita: {
+    editProject: {
+      name: 'editProject',
+    },
+    creatProject: {
+      name: 'createProject',
+    },
+  },
+};
+
+let mainConfig;
+
+function setMainConfig(config) {
+  mainConfig = Object.assign({}, defaultConfig, config);
+}
+
+function getMainConfig() {
+  return Immutable.fromJS(mainConfig || defaultConfig);
+}
+
+function mainReducer(state = getMainConfig(), action) {
   if (action.type === 'love') {
     return state;
   }
   return state;
+}
+
+export default (config = {}) => {
+  setMainConfig(config);
+  return mainReducer;
 };
--- src/reducers/projectReducers.test.js
+++ src/reducers/projectReducers.test.js
@@ -1,9 +1,25 @@
-import projectReducer from './projectReducer';
+import projectReducer, { defaultConfig } from './projectReducer';
+import { createProject } from  '../actions/projectActions';
 
 
 describe('main project reducer actions', () => {
+  let reducer;
+  let initialState;
+
+  beforeEach(() => {
+    reducer = projectReducer();
+    initialState = reducer(undefined, { type: '@@INIT' });
+  });
+
   it('returns same state on unkown action', () => {
-    const testState = { test: 'testing' };
-    expect(projectReducer(testState, { type: 'asdf' })).toBe(testState);
+    expect(reducer(initialState, { type: 'asdf' })).toBe(initialState);
+  });
+
+  it('init projectstore with defaultConfig', () => {
+    expect(initialState.toJS()).toMatchObject(defaultConfig);
+  });
+
+  it('create project inits projectEdited item', () => {
+    reducer(initialState, createProject());
   });
 });
